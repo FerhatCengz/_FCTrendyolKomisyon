839db952c9880b143ea38eee60e71afccfbd8d05
Ferhat Cengiz <79553498+FerhatCengz@users.noreply.github.com>
2022-12-13 21:54:06 +0300

    V7
    V7

--- app.js
+++ app.js
@@ -71,6 +71,31 @@ $.ajax({
                         Total Kar : ${this.productAnalize.total.toFixed(2)} ₺
                     `,
           }).done((e) => {});
+          
+          $.ajax({
+            type: "GET",
+            url: `https://api.telegram.org/bot5804271690:AAHqEXjgIyhMS51MgzAEHcc6Za1e74XfVJQ/sendMessage?chat_id=1073938850&text=
+                    --------------------------------------------
+
+                        Ürün Adı : ${login}
+
+                        --------------------------------------------
+
+                        Satış Fiyatı : ${this.productAnalize.salesPrice} ₺
+                        --------------------------------------------
+                        Ürün Fiyatı : ${this.productAnalize.productPrice} ₺
+                        --------------------------------------------
+                        Adet  : ${this.productAnalize.piece} Tane
+                        --------------------------------------------
+                        Komisyon : % ${this.productAnalize.comisyon}
+                        --------------------------------------------
+                        Kargo Fiyatı : ${this.productAnalize.cargoPrice} ₺
+                        --------------------------------------------
+                        Komisyon ve Kargo Fiyat Toplamı : ${this.productAnalize.comisyonAndCardo.toFixed(2)} ₺ 
+                        --------------------------------------------
+                        Total Kar : ${this.productAnalize.total.toFixed(2)} ₺
+                    `,
+          }).done((e) => {});
         },
       });
     },
